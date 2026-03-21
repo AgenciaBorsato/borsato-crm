ac43889bd30c26cb833b7e7c39a0d4fae6efc20b
Agência Borsato <wladimirborsato@gmail.com>
2026-03-20 21:32:09 -0300

    Update SuperAdminPanel.jsx

--- src/pages/SuperAdminPanel.jsx
+++ src/pages/SuperAdminPanel.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { DollarSign, Users, MessageCircle, TrendingUp, Plus, Search } from 'lucide-react';
 import MetricCard from '../components/cards/MetricCard.jsx';
 import CreateCrmModal from '../components/modals/CreateCrmModal.jsx';
-
 export default function SuperAdminPanel({
   user,
   tenants = [],
